322aac872d5a68b04ca4711afa396ebd3d6758f1
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-02-01 19:56:45 +0530

    feat: clean url ingest using bs4

--- url-ingest-service/main.py
+++ url-ingest-service/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, status, HTTPException
 import httpx
-
+from readability import Document
+from bs4 import BeautifulSoup
 app = FastAPI()
 
 @app.get("/api/health")
@@ -13,16 +14,18 @@ def check_url_ingest_service_health():
 @app.get("/api/url-ingest", status_code=status.HTTP_200_OK)
 async def get_url_ingest(resourceURL: str):
     async with httpx.AsyncClient() as client: # * AsyncClient -> used to keep the TCP connections open
-        # if AsyncClient is not used, it would open and close the connection for every lookup
+        # * if AsyncClient is not used, it would open and close the connection for every lookup
         try:
-            print("Resource URL -> ",resourceURL)
             response = await client.get(resourceURL)
             if(response.status_code!=200):
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
                     detail="Failed to fetch the resource"
                 )
-            return response.text
+            doc = Document(response.text)
+            soup = BeautifulSoup(doc.summary(), "html.parser")
+            text = soup.get_text(separator="\n")
+            return {"text" : text}
         
         except httpx.RequestError as exc:
             raise HTTPException(
