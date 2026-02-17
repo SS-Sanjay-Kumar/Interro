c85a269ff717596e99486bd799ca5bcee853a296
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-02-17 12:06:14 +0530

    fix: llm_calls endpoints

--- backend/api/v1/endpoints/llm_call.py
+++ backend/api/v1/endpoints/llm_call.py
@@ -5,6 +5,7 @@ from google import genai
 
 from models.llm import LLMCallRequest
 
+from services.exceptions import ServiceErrors
 from services.uploads import extract_data_from_file
 from services.url_ingest import get_url_ingest
 from services.yt_transcript import get_transcription_from_video_id
@@ -21,10 +22,11 @@ def check_health():
 @llmCallRouter.post("/", status_code=status.HTTP_200_OK)
 async def make_llm_call(req : LLMCallRequest):
 
-        fileContent = {"upload":""}
-        urlContent = {"url":""}
-        ytTranscript = {"transcript":""}
-
+        fileContent = ""
+        urlContent = ""
+        ytTranscript = ""
+        
+        # uploads
         if req.fileName!="":
             try:
                 fileContent = await extract_data_from_file(req.fileName)
@@ -38,16 +40,38 @@ async def make_llm_call(req : LLMCallRequest):
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail=f"Error deleting file: {e}"
             )
-        if req.resourceURL!="":
-            urlContent = await  get_url_ingest(req.resourceURL)
-        if req.ytVideoId!="":
-            ytTranscript = await get_transcription_from_video_id(req.ytVideoId)
+            except ServiceErrors as se:
+                raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Service Error: {se}"
+            )
         
+        # url ingest
+        if req.resourceURL!="":
+            try:
+                urlContent = await  get_url_ingest(req.resourceURL)
+            except ServiceErrors as se:
+                raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Service Error: {se}"
+            )
+
+        # yt transript
+        if req.ytVideoId!="":
+            try:
+                ytTranscript = await get_transcription_from_video_id(req.ytVideoId)
+            except ServiceErrors as se:
+                raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Service Error: {se}"
+            )
+        
+        # llm cal
         client = genai.Client()
 
         response = client.models.generate_content(
             model="gemini-3-flash-preview",
-            contents=f"Summarize the below text using analogy, max word limit 50\n {fileContent["upload"]} \n{urlContent["url"]} \n{ytTranscript["transcript"]}"
+            contents=f"Summarize the below text using analogy, max word limit 50\n {fileContent} \n{urlContent} \n{ytTranscript}"
         )
 
         print(response.text)
