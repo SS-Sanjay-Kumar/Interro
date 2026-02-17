3259092e4a40c5f0dadedf47b76f954065807bfe
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-02-17 15:51:11 +0530

    chore: frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Tailwind is working!
+      </h1>
+      <button className="btn btn-primary">
+        DaisyUI works
+      </button>
+    </>
+
+  )
+}
+
+export default App
